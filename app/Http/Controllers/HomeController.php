05423bccc938ed3a330125b487aa395594c42d9b
sardarietesam <sardarietesam812002@gmail.com>
2024-01-09 16:13:20 +0500

    first commit 2

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class HomeController extends Controller
+{
+    public function index()
+    {
+        return view ('home.userpage');
+    }
+
+
+    public function redirect()
+    {
+        $usertype=Auth::user()->usertype;
+        if($usertype=='1')
+        {
+            return view('admin.home');
+        }
+        else
+        {
+            return view('dashboard');
+        }
+        // return view('admin.home');
+        // return view('user.home');
+    }
+}
